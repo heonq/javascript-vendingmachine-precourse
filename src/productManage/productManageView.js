59f35fb788da9822048e68795c1b63b368336f24
heonq <gkagjsrb@naver.com>
2023-08-20 13:21:33 +0900

    feat: productManageView 객체 생성

--- src/productManage/productManageView.js
+++ src/productManage/productManageView.js
@@ -1,6 +1,22 @@
-import { productManageComponent, addProductTable } from './productManageComponent';
+import { productManageComponent } from './productManageComponent';
+import { CLASSES } from '../../utils/Constants';
 
-export const printProductManageComponent = (products) => {
-  $(`#${IDS.container}`).innerHTML = productManageComponent;
-  addProductTable(products);
+export const productManageView = {
+  addProductTable(products) {
+    const tableContent = products
+      .map((product) => {
+        return `<tr class="${CLASSES.productList}">
+          <td class="${CLASSES.productName}">${product.name}</td>
+          <td class="${CLASSES.productPrice}">${product.price}</td>
+          <td class="${CLASSES.productQuantity}">${product.quantity}</td>
+          </tr>`;
+      })
+      .join('');
+    $(`#${IDS.productManageTable}`).insertAdjacentHTML('beforeend', tableContent);
+  },
+
+  printProductManageComponent(products) {
+    $(`#${IDS.container}`).innerHTML = productManageComponent;
+    addProductTable(products);
+  },
 };
