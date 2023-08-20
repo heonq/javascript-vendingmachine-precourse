244fdd78c579b5713c32a1321ed310bc3aef1460
heonq <gkagjsrb@naver.com>
2023-08-20 16:00:24 +0900

    feat: tabButtonComponent에 container div 추가
    버튼을 누르면 container에 탭 별로 컴포넌트가 표시된다.

--- src/tabButtons/tabButtonsComponent.js
+++ src/tabButtons/tabButtonsComponent.js
@@ -4,4 +4,5 @@ export const tabButtonsComponent = `<div>
 <button id="${IDS.manageProductTabButton}">상품 관리</button>
 <button id="${IDS.chargingTabButton}">잔돈 충전</button>
 <button id= "${IDS.purchaseTabButton}">상품 구매</button>
-</div>`;
+</div>
+<div id="${IDS.container}"></div>`;
