2ca6c5d47d42cb27602c7449c761cb690ad4d4c6
heonq <gkagjsrb@naver.com>
2023-08-21 20:43:46 +0900

    feat: 앱 실행 시 화면 렌더하는 기능 구현

--- src/VendingMachineController.js
+++ src/VendingMachineController.js
@@ -1,7 +1,28 @@
 import { tabButtonsView } from './tabButtons/tabButtonsView.js';
+import { productManageView } from './productManage/productManageView.js';
+import ProductManage from './productManage/productManage.js';
+import { $ } from '../utils/index.js';
+import { IDS } from '../utils/Constants.js';
 
 export default class VendingMachineController {
+  #productManage;
+
+  constructor() {
+    this.#productManage = new ProductManage();
+  }
+
   init() {
+    this.renderMainComponents();
+    this.renderProductManage();
+  }
+
+  renderMainComponents() {
     tabButtonsView.printTabButtons();
+  }
+
+  renderProductManage() {
+    this.#productManage.getFromStore();
+    productManageView.printProductManageComponent(this.#productManage.getProducts());
+    productManageView.handleSubmit(this.#productManage);
   }
 }
--- src/productManage/productManage.js
+++ src/productManage/productManage.js
@@ -5,19 +5,27 @@ import { Validator } from '../../utils/Validator.js';
 
 export default class ProductManage {
   #products;
+
   constructor() {
     this.#products = [];
   }
+  getFromStore() {
+    if (Store.getItem(CONSTANTS.productsKey)) this.#products = Store.getItem(CONSTANTS.productsKey);
+  }
 
   storeProduct() {
-    if (Store.getItem(CONSTANTS.productsKey)) this.#products = this.getFromStore();
     const [name, price, quantity] = [
       $(`#${IDS.productNameInput}`).value,
       $(`#${IDS.productPriceInput}`).value,
       $(`#${IDS.productQuantityInput}`).value,
     ];
     if (!Validator.validateProductPrice(price)) return;
+    if (name === '' || price === '' || quantity === '') return;
     this.#products.push({ name, price, quantity });
     Store.setItem(CONSTANTS.productsKey, this.#products);
+  }
+
+  getProducts() {
+    return this.#products;
   }
 }
