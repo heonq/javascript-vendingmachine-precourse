600717af1a08d29976af02b2a192e90aacf5a1b0
heonq <gkagjsrb@naver.com>
2023-08-21 16:00:04 +0900

    fix: 상품 이름 인풋창 ID 수정
    product-add-button -> product-name-input

--- src/productManage/productManageComponent.js
+++ src/productManage/productManageComponent.js
@@ -2,8 +2,8 @@ import { CONSTANTS, IDS } from '../../utils/Constants.js';
 
 export const productManageComponent = `<div id="product-manage"><p>${CONSTANTS.addProduct}</p>
     <form id="${IDS.productManageForm}">
-    <input id="${IDS.addProductButton}" type="text" placeholder="${CONSTANTS.productName}" />
-    <input id="${IDS.productPriceInput}" type="number" placeholder="${CONSTANTS.quantity}" />
+    <input id="${IDS.productNameInput}" type="text" placeholder="${CONSTANTS.productName}"/>
+    <input id="${IDS.productPriceInput}" type="number" placeholder="${CONSTANTS.price}"/>
     <input id="${IDS.productQuantityInput}" type="number" placeholder="${CONSTANTS.quantity}"/>
     <button id="${IDS.addProductButton}">${CONSTANTS.add}</button>
     </form>
