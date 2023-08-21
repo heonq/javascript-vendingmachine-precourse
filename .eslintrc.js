4c210973f6ea07a879aac28e17ee942f00b88b58
heonq <gkagjsrb@naver.com>
2023-08-21 20:42:22 +0900

    style: single quote 적용
    .eslintrc.js 파일 single quote 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,20 +7,20 @@ module.exports = {
   parserOptions: {
     ecmaVersion: latest,
   },
-  extends: ["airbnb"],
+  extends: ['airbnb'],
   rules: {
-    "linebreak-style": ["error", "unix"],
+    'linebreak-style': ['error', 'unix'],
 
-    "max-depth": ["error", 2],
-    "max-lines-per-function": ["error", 10],
-    "max-params": ["error", 3],
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', 10],
+    'max-params': ['error', 3],
   },
   overrides: [
     {
-      files: ["__tests__/**/*.js"],
+      files: ['__tests__/**/*.js'],
       rules: {
-        "max-lines-per-function": "off",
-        "no-new": "off",
+        'max-lines-per-function': 'off',
+        'no-new': 'off',
       },
     },
   ],
